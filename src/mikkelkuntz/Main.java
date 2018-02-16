6577a00f57f2d6e3e5ce3de921cd9a9401972365
BuildTools <unconfigured@null.spigotmc.org>
2018-02-17 00:40:39 +0100

    added base to work with... nothing works

---
+++ src/mikkelkuntz/Main.java
@@ -0,0 +1,28 @@
+package mikkelkuntz;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mikkelkuntz/PrimaryScene.fxml"));
+
+            primaryStage.setTitle("Super Window");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/mikkelkuntz/PrimarySceneController.java
@@ -0,0 +1,87 @@
+package mikkelkuntz;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class PrimarySceneController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button buttonBatFolder;
+
+    @FXML
+    private Button buttonPoE;
+
+    @FXML
+    private Button buttonRL;
+
+    @FXML
+    private Label labelPingGoogle;
+
+    @FXML
+    private Label labelPingRouter;
+
+
+    @FXML
+    public void initialize() {
+        assert buttonBatFolder != null : "fx:id=\"buttonBatFolder\" was not injected: check your FXML file 'PrimaryScene.fxml'.";
+        assert buttonPoE != null : "fx:id=\"buttonPoE\" was not injected: check your FXML file 'PrimaryScene.fxml'.";
+        assert buttonRL != null : "fx:id=\"buttonRL\" was not injected: check your FXML file 'PrimaryScene.fxml'.";
+        assert labelPingGoogle != null : "fx:id=\"labelPingGoogle\" was not injected: check your FXML file 'PrimaryScene.fxml'.";
+        assert labelPingRouter != null : "fx:id=\"labelPingRouter\" was not injected: check your FXML file 'PrimaryScene.fxml'.";
+
+
+    }
+
+    @FXML
+    public void openRL(javafx.event.ActionEvent event) {
+        //TODO: Opens Rocket League
+        //start steam://rungameid/252950
+
+        /*
+        try {
+            Runtime.getRuntime().exec("ping");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+        /*
+        try {
+            Process process = new ProcessBuilder("start steam://rungameid/252950").start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //Process p = pb.start();
+           */
+    }
+
+    @FXML
+    public void openPoE(javafx.event.ActionEvent event) {
+        //TODO: Opens Path of Exile
+        //start steam://rungameid/238960
+    }
+
+    @FXML
+    public void pingRouter(javafx.event.ActionEvent event){
+        //TODO : Create
+        //https://www.mkyong.com/java/how-to-execute-shell-command-from-java/
+        //MAYBE ANTOHER SOURCE?
+    }
+
+
+
+}
